bd5203869d20da969e12d407331d5e8ff2712bf2
Alexander Brigham <izalizard@gmail.com>
2015-05-30 21:39:04 -0400

    Fixed MongoDB refusing external connections

--- vagrantScripts/db.sh
+++ vagrantScripts/db.sh
@@ -6,3 +6,7 @@
 
 # Install required software
 sudo apt-get install -y mongodb python
+
+# Configure MongoDB to accept external connections
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongodb.conf
+sudo service mongodb restart
